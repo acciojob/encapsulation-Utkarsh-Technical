cc683fe9964d66ce8426190292cd62bf217fc52d
Utkarsh Shukla <63734211+Utkarsh-Technical@users.noreply.github.com>
2023-04-16 22:39:59 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly rw = new RWOnly();
+      // rw.name="Utkarsh";
+      // 'name' has private access in 'com.driver.RWOnly'
+      String name= "AccioJob";
+      rw.setter(name);
+      String str = rw.getter();
+      System.out.println(str);
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    void setter(String name){
+        this.name=name;
+    }
+    String getter(){
+        return name;
+    }
+
+
 }
